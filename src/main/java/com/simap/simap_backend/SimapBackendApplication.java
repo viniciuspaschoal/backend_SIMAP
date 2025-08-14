250579c82390091be0a6c70e9af86787d74f8f46
viniciuspaschoal <viniciuspaschoal1847@gmail.com>
2025-08-13 22:54:17 -0300

    Add principal files to main

---
+++ src/main/java/com/simap/simap_backend/SimapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.simap.simap_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimapBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simap/simap_backend/SimapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simap.simap_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
