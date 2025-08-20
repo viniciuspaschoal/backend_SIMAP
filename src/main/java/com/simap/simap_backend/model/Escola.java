670f27752c429c62f18b69e697e664d8c3d5f3fc
viniciuspaschoal <viniciuspaschoal1847@gmail.com>
2025-08-20 18:45:36 -0300

    Ajust stores schools with address and phone; PK cod_escola.

--- src/main/java/com/simap/simap_backend/model/Escola.java
+++ src/main/java/com/simap/simap_backend/model/Escola.java
@@ -3,6 +3,7 @@ package com.simap.simap_backend.model;
 import jakarta.persistence.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "escola")
@@ -10,7 +11,7 @@ public class Escola {
 
     @Id
     @Column(name = "codEscola", length = 7)
-    private String codEscola;
+    private String codEscola; //chave primária
 
     @Column(name = "nome_escola", length = 200)
     private String nomeEscola;
@@ -43,4 +44,121 @@ public class Escola {
     @OneToMany(mappedBy = "escola")
     private List<Turma> turmas;
 
+    public Escola(String codEscola, String nomeEscola, String telefone,
+                  String endRua, String endBairro, String endNumero,
+                  String endCidade, String endCep, String endEstado,
+                  String endCompleto, List<Turma> turmas) {
+        this.codEscola = codEscola;
+        this.nomeEscola = nomeEscola;
+        this.telefone = telefone;
+        this.endRua = endRua;
+        this.endBairro = endBairro;
+        this.endNumero = endNumero;
+        this.endCidade = endCidade;
+        this.endCep = endCep;
+        this.endEstado = endEstado;
+        this.endCompleto = endCompleto;
+        this.turmas = turmas;
+    }
+
+    public String getCodEscola() {
+        return codEscola;
+    }
+
+    public void setCodEscola(String codEscola) {
+        this.codEscola = codEscola;
+    }
+
+    public String getNomeEscola() {
+        return nomeEscola;
+    }
+
+    public void setNomeEscola(String nomeEscola) {
+        this.nomeEscola = nomeEscola;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndRua() {
+        return endRua;
+    }
+
+    public void setEndRua(String endRua) {
+        this.endRua = endRua;
+    }
+
+    public String getEndBairro() {
+        return endBairro;
+    }
+
+    public void setEndBairro(String endBairro) {
+        this.endBairro = endBairro;
+    }
+
+    public String getEndNumero() {
+        return endNumero;
+    }
+
+    public void setEndNumero(String endNumero) {
+        this.endNumero = endNumero;
+    }
+
+    public String getEndCidade() {
+        return endCidade;
+    }
+
+    public void setEndCidade(String endCidade) {
+        this.endCidade = endCidade;
+    }
+
+    public String getEndEstado() {
+        return endEstado;
+    }
+
+    public void setEndEstado(String endEstado) {
+        this.endEstado = endEstado;
+    }
+
+    public String getEndCep() {
+        return endCep;
+    }
+
+    public void setEndCep(String endCep) {
+        this.endCep = endCep;
+    }
+
+    public String getEndCompleto() {
+        return endCompleto;
+    }
+
+    public void setEndCompleto(String endCompleto) {
+        this.endCompleto = endCompleto;
+    }
+
+    public List<Turma> getTurmas() {
+        return turmas;
+    }
+
+    public void setTurmas(List<Turma> turmas) {
+        this.turmas = turmas;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Escola escola = (Escola) o;
+        return Objects.equals(codEscola, escola.codEscola) && Objects.equals(nomeEscola, escola.nomeEscola) && Objects.equals(telefone, escola.telefone) && Objects.equals(endRua, escola.endRua) && Objects.equals(endBairro, escola.endBairro) && Objects.equals(endNumero, escola.endNumero) && Objects.equals(endCidade, escola.endCidade) && Objects.equals(endEstado, escola.endEstado) && Objects.equals(endCep, escola.endCep) && Objects.equals(endCompleto, escola.endCompleto) && Objects.equals(turmas, escola.turmas);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codEscola, nomeEscola, telefone, endRua, endBairro, endNumero, endCidade, endEstado, endCep, endCompleto, turmas);
+    }
 }
