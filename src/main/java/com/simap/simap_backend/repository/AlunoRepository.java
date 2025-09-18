1a3cca9183fdae16e3173d463af02d0bea59fc63
viniciuspaschoal <viniciuspaschoal1847@gmail.com>
2025-09-18 16:02:27 -0300

    Developed searches for student's school, grade, class and period

--- src/main/java/com/simap/simap_backend/repository/AlunoRepository.java
+++ src/main/java/com/simap/simap_backend/repository/AlunoRepository.java
@@ -4,8 +4,11 @@ import com.simap.simap_backend.model.Aluno;
 import com.simap.simap_backend.model.AlunoTurma;
 import com.simap.simap_backend.model.Turma;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AlunoRepository extends JpaRepository<Aluno, String> {
 
@@ -19,8 +22,52 @@ public interface AlunoRepository extends JpaRepository<Aluno, String> {
     List<Aluno> findByAlunoTurmas_Turma_CodTurma(String codTurma);
 
     //Busca aluno por RA - deve ser único
-    Aluno findByRa(String ra);
+    Optional<Aluno> findByRa(String ra);
 
     //Busca alunos por declaração
     List<Aluno> findByDeclaracao_CodDeclaracao(String codDeclaracao);
+
+    //Busca escola do aluno
+    @Query("SELECT e.nomeEscola FROM Aluno a " +
+            "JOIN a.alunoTurmas at " +
+            "JOIN at.turma t " +
+            "JOIN t.escola e " +
+            "WHERE a.ra = :ra " +
+            "AND t.anoLetivo = 2025")
+    String findEscolaByRa(@Param("ra") String ra);
+    //SELECT
+    //    a.nome_aluno, a.ra, e.nome_escola, t.anoletivo_r
+    //FROM aluno AS a
+    //INNER JOIN
+    //    aluno_turma AS alt ON a.cod_aluno = alt.cod_aluno
+    //INNER JOIN
+    //    turma AS t ON alt.cod_turma = t.cod_turma
+    //INNER JOIN
+    //    escola AS e ON t.cod_escola = e.cod_escola
+    //WHERE a.ra = '110028366' AND t.anoletivo_r = 2025
+
+    @Query("SELECT t.serie " +
+            "FROM Aluno a " +
+            "JOIN a.alunoTurmas at " +
+            "JOIN at.turma t " +
+            "WHERE a.ra = :ra " +
+            "AND t.anoLetivo = 2025")
+    String findSerieByRa(@Param("ra") String ra);
+
+    @Query("SELECT t.turma " +
+            "FROM Aluno a " +
+            "JOIN a.alunoTurmas at " +
+            "JOIN at.turma t " +
+            "WHERE a.ra = :ra " +
+            "AND t.anoLetivo = 2025")
+    String findTurmaByRa(@Param("ra") String ra);
+
+    @Query("SELECT t.periodoRegular " +
+            "FROM Aluno a " +
+            "JOIN a.alunoTurmas at " +
+            "JOIN at.turma t " +
+            "WHERE a.ra = :ra " +
+            "AND t.anoLetivo = 2025")
+    String findPeriodoByRa(@Param("ra") String ra);
+
 }
