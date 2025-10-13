f6331381cafe0820b7061a7ffd82cc356f9b1a4a
viniciuspaschoal <viniciuspaschoal1847@gmail.com>
2025-10-13 20:16:32 -0300

    Added POST /api/aluno/filtro endpoint to allow dynamic student filtering by multiple criteria (school, grade, class, and diagnostic hypotheses)

--- src/main/java/com/simap/simap_backend/controller/AlunoController.java
+++ src/main/java/com/simap/simap_backend/controller/AlunoController.java
@@ -1,12 +1,14 @@
 package com.simap.simap_backend.controller;
 
 import com.simap.simap_backend.dto.AlunoDTO;
+import com.simap.simap_backend.dto.FiltroAlunosRequest;
+import com.simap.simap_backend.dto.ResultadoFiltrosDTO;
 import com.simap.simap_backend.service.AlunoService;
+import com.simap.simap_backend.service.FiltroAlunoService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 //Combina @Controller + @ResponseBody, então o retorno dos métodos vira JSON automaticamente.
 @RestController
@@ -14,9 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AlunoController {
 
     private final AlunoService alunoService;
+    private final FiltroAlunoService filtroAlunoService;
 
-    public AlunoController(AlunoService alunoService){
+    public AlunoController(AlunoService alunoService, FiltroAlunoService filtroAlunoService){
         this.alunoService = alunoService;
+        this.filtroAlunoService = filtroAlunoService;
     }
 
     // GET /api/aluno/ra/{ra}
@@ -27,4 +31,19 @@ public class AlunoController {
 //        return ResponseEntity.ok(dto);                  //retorna DTO + JSON
         return ResponseEntity.ok(alunoService.obterCompletoporRA(ra));
     }
+
+    // Metodo POST para receber os filtros
+    @PostMapping("/filtro")
+    public ResponseEntity<List<ResultadoFiltrosDTO>> receberFiltragem(@RequestBody FiltroAlunosRequest filtros) {
+        // Log opcional (só pra acompanhar no console)
+        System.out.println("Filtros Recebidos: " + filtros);
+
+        //Chama o Service, que faz a busca no banco e monta os DTOs
+        List<ResultadoFiltrosDTO> resultado = filtroAlunoService.buscarAlunosPorFiltro(filtros);
+
+        // Retorna uma confirmação de que os filtros foram recebidos com sucesso
+        return ResponseEntity.ok(resultado);
+    }
+
+
 }
